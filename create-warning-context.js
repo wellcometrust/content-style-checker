5a70834ff7e4b23a0427ef8ce53fff856afee8f8
jakubfiala <mu201jf@gold.ac.uk>
2017-04-11 15:39:13 +0100

    pimped up context

--- create-warning-context.js
+++ create-warning-context.js
@@ -1,7 +1,17 @@
 const createWarningContext = (match, sentence) => {
-  const contextStart = sentence.indexOf(match) - 12;
-  const contextEnd = sentence.indexOf(match) + match.length + 12;
-  return `… ${sentence.slice(contextStart, contextEnd)} …`;
+  const contextStart = sentence.indexOf(match) - 20;
+  const contextEnd = sentence.indexOf(match) + match.length + 20;
+
+  const fullContext = sentence.slice(contextStart, contextEnd);
+  const context = [
+    fullContext.slice(0, fullContext.indexOf(match)),
+    '<strong>',
+    match, ' ',
+    '</strong>',
+    fullContext.slice(fullContext.indexOf(match) + match.length + 1)
+  ].join('');
+
+  return `… ${context} …`;
 }
 
 module.exports = createWarningContext;
